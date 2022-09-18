dd07d64c5e2af555228b3cb728868913bd556f84
Sumaia Akter <sumaiaakter@Sumaias-MBP.lan>
2022-09-18 02:37:58 -0400

    added

--- promises.js
+++ promises.js
@@ -1,20 +1,47 @@
 const hasMeeting = false;
 const promise = new Promise((resolve, reject) => {
     if (!hasMeeting) {
-        const meeting = {
+        const meetingDetails = {
             name: "technical meetin",
             location: "zoom",
             time: "10am"
         };
-        resolve(meeting);
+        resolve(meetingDetails);
     } else {
         reject(new Error("done"))
     }
 });
-promise.then((result) => {
+
+// declare multiple promise-------
+
+// const addToCalendar = (meeting) => {
+//     return new Promise((resolve, reject) => {
+//         const calendar = `${meeting.name}`;
+//         resolve(calendar)
+//     })
+// };
+//OR----------thus is another way
+const addToCalendar = (meetingDetails) => {
+    const calendar = `meeting has been held on ${meetingDetails.location}`
+    return Promise.resolve(calendar);
+    
+};
+promise
+    .then(addToCalendar)
+    .then((result) => {
     // get resolve data here
     console.log(JSON.stringify(result))
 }).catch((error) => {
     // get rejected data here
     console.log(error.message)
+});
+
+const promise1 = Promise.resolve('promise 1 resolved');
+const promise2 = new Promise((resolve, reject) => {
+    setTimeout(() => {
+        resolve('promise 2 resolved')
+    }, 2000);
+})
+Promise.all([promise1, promise2]).then((res) => {
+    console.log(res);
 })
